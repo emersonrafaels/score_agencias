e73bdd0d644263aedd0d346db6769bc16377c080
Emerson Rafael <emersona7x@hotmail.com>
2024-06-21 04:18:28 -0300

    [CHG] INCLUINDO O SCORE POR TEMPO

--- model_score/score.py
+++ model_score/score.py
@@ -3,14 +3,26 @@ from pydantic import BaseModel, validator
 from typing import Dict, Union
 
 
-class Weight(BaseModel):
+class Weight_Float(BaseModel):
     column: str
     categories: Dict[Union[str, bool], float]
 
     @validator("categories")
     def check_categories_values(cls, v):
+        # O PESO DEVE SER UM VALOR ENTRE 0 E 1
         if any(weight < 0 or weight > 1 for weight in v.values()):
             raise ValueError("Os pesos das categorias devem estar entre 0 e 1.")
+        return v
+
+class Weight_Str(BaseModel):
+    column: str
+    categories: Dict[Union[str, bool], str]
+
+    @validator("categories")
+    def check_categories_values(cls, v):
+        # O PESO DEVE SER UM VALOR ENTRE 0 E 1
+        if any(not isinstance(weight, str) for weight in v.values()):
+            raise ValueError("Os pesos das categorias devem ser string")
         return v
 
 
@@ -21,7 +33,7 @@ class Score:
         """
         self.weights = {}
 
-    def insert(self, weight: Dict):
+    def insert_values(self, weight: Dict):
         """
         INSERE UM DICIONÁRIO DE PESOS NA INSTÂNCIA DE SCORE, VALIDADO PELO MODELO PYDANTIC.
 
@@ -55,7 +67,32 @@ class Score:
             BOOL: TRUE SE O PESO FOI INSERIDO COM SUCESSO, FALSE CASO CONTRÁRIO.
         """
         try:
-            valid_weight = Weight(**weight)
+            valid_weight = Weight_Float(**weight)
+            self.weights[valid_weight.column] = valid_weight.categories
+            logger.info(
+                f"PESO INSERIDO COM SUCESSO - COLUMN: {valid_weight.column}, CATEGORIES: {valid_weight.categories}"
+            )
+            return True
+        except Exception as e:
+            logger.error(f"PESO NÃO INSERIDO COM SUCESSO - {str(e)}")
+            return False
+
+    def insert_str(self, weight: Dict):
+        """
+        INSERE UM DICIONÁRIO DE PESOS NA INSTÂNCIA DE SCORE, VALIDADO PELO MODELO PYDANTIC.
+
+        O PESO DEVE SER UM DICIONÁRIO QUE SEGUE O PADRÃO, VALIDADO PELO MODELO `Weight`:
+        - "COLUMN": NOME DA COLUNA PARA A QUAL O PESO SERÁ APLICADO,
+        - "CATEGORIES": UM SUBDICIONÁRIO COM CATEGORIAS DA COLUNA E SEUS PESOS ASSOCIADOS.
+
+        PARÂMETROS:
+            WEIGHT (DICT): O PESO A SER INSERIDO, CONFORME O PADRÃO DESCRITO.
+
+        RETORNA:
+            BOOL: TRUE SE O PESO FOI INSERIDO COM SUCESSO, FALSE CASO CONTRÁRIO.
+        """
+        try:
+            valid_weight = Weight_Str(**weight)
             self.weights[valid_weight.column] = valid_weight.categories
             logger.info(
                 f"PESO INSERIDO COM SUCESSO - COLUMN: {valid_weight.column}, CATEGORIES: {valid_weight.categories}"
